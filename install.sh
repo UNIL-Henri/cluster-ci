a22c1c22e4e340ad3df2f30077d7ebf04eed24fb
hjamet <henri.jamet@unil.ch>
2026-04-12 14:19:34 +0200

    fix: use absolute path for global orchestrator script

--- install.sh
+++ install.sh
@@ -32,7 +32,7 @@ jobs:
           fetch-depth: 0
 
       - name: Run Orchestrator
-        run: cluster-ci-run "\${{ github.repository }}" "\${{ github.ref_name }}" "\${{ secrets.GITHUB_TOKEN }}"
+        run: /usr/local/bin/cluster-ci-run "\${{ github.repository }}" "\${{ github.ref_name }}" "\${{ secrets.GITHUB_TOKEN }}"
 EOF
 
 # 3. Injection du fichier de configuration .cluster-ci
